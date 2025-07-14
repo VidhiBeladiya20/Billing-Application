ecd485bc12f8b5a5c5ad6f12ca7c4163af2dc453
VidhiBeladiya20 <vidhibeladiya307@gmail.com>
2025-07-14 23:34:12 +0530

    Initial Commit

---
+++ src/main/java/com/example/Bill_System/BillSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bill_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSystemApplication.class, args);
+	}
+
+}
